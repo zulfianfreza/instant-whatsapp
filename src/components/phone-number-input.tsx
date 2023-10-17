13d63ee539b826e07aa7adc9e39c1f7d6eb84622
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2023-10-17 14:12:43 +0700

    update number input

--- src/components/phone-number-input.tsx
+++ src/components/phone-number-input.tsx
@@ -36,6 +36,15 @@ export default function PhoneNumberInput({
     code: "ID",
   });
   const [number, setNumber] = React.useState("");
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
+  const [buttonWidth, setButtonWidth] = React.useState(0);
+
+  React.useEffect(() => {
+    if (buttonRef.current) {
+      setButtonWidth(buttonRef.current.offsetWidth);
+    }
+  }, [countryCode]);
+
   return (
     <div className=" gap-y-2 flex flex-col">
       <label
@@ -44,54 +53,10 @@ export default function PhoneNumberInput({
       >
         Phone Number *
       </label>
-      <div className=" w-full h-12 ring-2 ring-neutral-200 dark:ring-neutral-800 rounded-lg overflow-hidden flex items-center peer-focus/number:bg-primary">
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              role="combobox"
-              aria-expanded={open}
-              className=" h-12 border-none transition-none bg-transparent hover:bg-transparent rounded-none dark:bg-neutral-900 dark:hover:bg-neutral-900 font-normal"
-            >
-              {countryCode
-                ? countryCodes.find(
-                    (country) => country.name === countryCode?.name
-                  )?.dial_code
-                : "Select framework..."}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] p-0">
-            <Command>
-              <CommandInput placeholder="Search country..." className="h-9" />
-              <CommandEmpty>No framework found.</CommandEmpty>
-              <CommandGroup>
-                <div className=" h-60 overflow-y-scroll">
-                  {countryCodes.map((country, index) => (
-                    <CommandItem
-                      key={index}
-                      onSelect={() => {
-                        setCountryCode(country);
-                        setOpen(false);
-                        setValue(country.dial_code + number);
-                      }}
-                      className={cn({
-                        "bg-primary/20": country == countryCode,
-                      })}
-                    >
-                      <div className=" w-full flex justify-between">
-                        <span>{country.name}</span>
-                        <span>{country.dial_code}</span>
-                      </div>
-                    </CommandItem>
-                  ))}
-                </div>
-              </CommandGroup>
-            </Command>
-          </PopoverContent>
-        </Popover>
+      <div className=" w-full h-12 relative">
         <input
           type="text"
-          className=" w-full h-12 focus:outline-none text-sm text-neutral-900 dark:ring-neutral-800 peer/number dark:bg-neutral-900 dark:text-neutral-100"
+          className=" w-full h-12 focus:outline-none focus:ring-primary overflow-hidden ring-2 rounded-lg ring-neutral-200 text-sm text-neutral-900 dark:ring-neutral-800 dark:bg-neutral-900 dark:text-neutral-100"
           value={number}
           onChange={(e) => {
             const result = e.target.value.replace(/\D/g, "");
@@ -99,9 +64,56 @@ export default function PhoneNumberInput({
             setNumber(result.toString());
             setValue(countryCode?.dial_code + result);
           }}
+          style={{ paddingLeft: `${buttonWidth}px` }}
         />
+        <div className=" absolute left-0 top-0">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                ref={buttonRef}
+                className=" h-12 border-none transition-none bg-transparent hover:bg-transparent rounded-none dark:bg-neutral-900 dark:hover:bg-neutral-900 font-normal"
+              >
+                {countryCode
+                  ? countryCodes.find(
+                      (country) => country.name === countryCode?.name
+                    )?.dial_code
+                  : "Select country..."}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput placeholder="Search country..." className="h-9" />
+                <CommandEmpty>No country found.</CommandEmpty>
+                <CommandGroup>
+                  <div className=" h-60 overflow-y-scroll">
+                    {countryCodes.map((country, index) => (
+                      <CommandItem
+                        key={index}
+                        onSelect={() => {
+                          setCountryCode(country);
+                          setOpen(false);
+                          setValue(country.dial_code + number);
+                        }}
+                        className={cn({
+                          "bg-primary/20": country == countryCode,
+                        })}
+                      >
+                        <div className=" w-full flex justify-between">
+                          <span>{country.name}</span>
+                          <span>{country.dial_code}</span>
+                        </div>
+                      </CommandItem>
+                    ))}
+                  </div>
+                </CommandGroup>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
-      <p className=" peer-focus/number:block hidden">Test</p>
     </div>
   );
 }
